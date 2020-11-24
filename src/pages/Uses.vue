a3dd78325808a87b0d307fcb8552e77a7d3b0959
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2020-11-23 18:43:09 -0700

    added uses content

--- src/pages/Uses.vue
+++ src/pages/Uses.vue
@@ -13,13 +13,30 @@
         <a href="https://uses.tech/">here</a>.
       </p>
       <div
-        class="text-2xl font-bold flex flex-col sm:flex-row justify-between w-full sm:w-3/5 text-center sm:text-left py-10"
+        class="flex flex-col sm:flex-row text-center justify-between w-full sm:text-left py-10"
       >
         <div class="leading-snug">
-          <h1>Hardware</h1>
+          <h1 class="text-2xl font-bold">Hardware</h1>
+          <ul class="list-disc">
+            <li>MacBook Pro 15", Late 2017</li>
+            <li>Iphone X</li>
+          </ul>
         </div>
-        <div class="leading-snug">Development</div>
-        <div class="leading-snug">Other tools</div>
+        <div class="leading-snug">
+          <h1 class="text-2xl font-bold">Development</h1>
+          <ul class="list-disc">
+            <li>Editor: Visual Studio Code</li>
+            <li>Main browser: Chrome</li>
+            <li>Terminal: iTerm2</li>
+          </ul>
+        </div>
+        <div class="leading-snug">
+          <h1 class="text-2xl font-bold">Other tools</h1>
+          <ul class="list-disc">
+            <li>Note Taking: Notion</li>
+            <li>Music: Spotify</li>
+          </ul>
+        </div>
       </div>
     </div>
   </Layout>
